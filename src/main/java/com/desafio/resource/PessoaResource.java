4192bf491f79f1d099d00fc815ccf653c5485e22
cleiton <djcleitonduarte@hotmail.com>
2021-04-09 16:52:51 -0400

    Criado um método buscar no endpoint '/pessoas'

---
+++ src/main/java/com/desafio/resource/PessoaResource.java
@@ -0,0 +1,19 @@
+package com.desafio.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafio.domain.Pessoa;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+
+	@GetMapping
+	public ResponseEntity<Pessoa> burcar(){
+		Pessoa p1 = new Pessoa(1, "Cleiton", "67 99999-9999");
+		return ResponseEntity.ok().body(p1);
+	}
+}
