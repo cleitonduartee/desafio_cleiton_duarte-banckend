0efc6fd7d526cca4f6a8ed778b7cc2b19f750b8d
cleiton <djcleitonduarte@hotmail.com>
2021-04-09 16:24:36 -0400

    Iniciando projeto

---
+++ src/main/java/com/desafio/SistemaDoadorApplication.java
@@ -0,0 +1,13 @@
+package com.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDoadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDoadorApplication.class, args);
+	}
+
+}
