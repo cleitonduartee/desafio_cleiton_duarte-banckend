c0a5311840404cfc8abc1757e984977516ff9b1c
cleiton <djcleitonduarte@hotmail.com>
2021-04-09 17:57:58 -0400

    Capturando exception na Camada de Resource

---
+++ src/main/java/com/desafio/resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.desafio.resource.exception;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.desafio.service.exception.NotFoundResourceException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(NotFoundResourceException.class)
+	public ResponseEntity<StandardError> notFoundResource(NotFoundResourceException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		String error = "Erro na requisição.";
+		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
